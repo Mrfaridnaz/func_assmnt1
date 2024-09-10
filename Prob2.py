f1ba7aabe3de6b97435c093dba692ed37b623e0b
mrfaridnaz <mrfad8392@gmail.com>
2024-09-10 23:59:04 +0530

    add solution2

---
+++ Prob2.py
@@ -0,0 +1,18 @@
+# Can a lambda function in Python have multiple arguments? If yes, how can you define and use
+# them?
+# Yes, Explaination in text2
+
+# Lambda function with two arguments
+multiply = lambda x, y: x * y
+result = multiply(5, 10)
+print(result)  # Output: 50
+
+
+# Lambda function with three arguments
+sum_three_numbers = lambda a, b, c: a + b + c
+result = sum_three_numbers(1, 2, 3)
+print(result)  # Output: 6
+
+
+
+
